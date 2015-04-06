5317f1834894abda140bc32eb2414fbefbdcab85
Mathias Nestler <mathias.nestler@barzahlen.de>
2015-04-06 22:09:42 +0200

    added really ugly function to read wav files from memory

--- example.go
+++ example.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
-	"github.com/vova616/go-openal/openal"
+	"github.com/MMore/go-openal/openal"
 	"math"
 	"os"
 	"time"
+	"bytes"
 )
 
 type Format struct {
@@ -98,6 +99,67 @@ func ReadWavFile(path string) (*Format, []byte, error) {
 	return &format, wavData, nil
 }
 
+func ReadWavFromMemory(memory []byte) (*Format, []byte, error) {
+	f := bytes.NewReader(memory)
+	var buff [4]byte
+	f.Read(buff[:4])
+
+	if string(buff[:4]) != "RIFF" {
+		return nil, nil, fmt.Errorf("Not a WAV file.\n")
+	}
+
+	var size int32
+	binary.Read(f, binary.LittleEndian, &size)
+
+	f.Read(buff[:4])
+
+	if string(buff[:4]) != "WAVE" {
+		return nil, nil, fmt.Errorf("Not a WAV file.\n")
+	}
+
+	f.Read(buff[:4])
+
+	if string(buff[:4]) != "fmt " {
+		return nil, nil, fmt.Errorf("Not a WAV file.\n")
+	}
+
+	binary.Read(f, binary.LittleEndian, &size)
+
+	var format Format
+
+	switch size {
+	case 16:
+		binary.Read(f, binary.LittleEndian, &format)
+	case 18:
+		var f2 Format2
+		binary.Read(f, binary.LittleEndian, &f2)
+		format = f2.Format
+	case 40:
+		var f3 Format3
+		binary.Read(f, binary.LittleEndian, &f3)
+		format = f3.Format
+	}
+
+	f.Read(buff[:4])
+
+	if string(buff[:4]) != "data" {
+		return nil, nil, fmt.Errorf("Not supported WAV file.\n")
+	}
+
+	binary.Read(f, binary.LittleEndian, &size)
+
+	wavData := make([]byte, size)
+	n, e := f.Read(wavData)
+	if e != nil {
+		return nil, nil, fmt.Errorf("Cannot read WAV data.\n")
+	}
+	if int32(n) != size {
+		return nil, nil, fmt.Errorf("WAV data size doesnt match.\n")
+	}
+
+	return &format, wavData, nil
+}
+
 func Period(freq int, samples int) float64 {
 	return float64(freq) * 2 * math.Pi * (1 / float64(samples))
 }
@@ -106,24 +168,15 @@ func TimeToData(t time.Duration, samples int, channels int) int {
 	return int((float64(samples)/(1/t.Seconds()))+0.5) * channels
 }
 
-func main() {
+func PlaySoundFromMemory(memory []byte) {
 	device := openal.OpenDevice("")
 	context := device.CreateContext()
 	context.Activate()
 
-	//listener := new(openal.Listener)
-	//listener.
-
 	source := openal.NewSource()
-	source.SetPitch(1)
-	source.SetGain(1)
-	source.SetPosition(0, 0, 0)
-	source.SetVelocity(0, 0, 0)
-	source.SetLooping(false)
-
 	buffer := openal.NewBuffer()
 
-	format, data, err := ReadWavFile("welcome.wav")
+	format, data, err := ReadWavFromMemory(memory)
 	if err != nil {
 		panic(err)
 	}
@@ -138,15 +191,12 @@ func main() {
 	source.SetBuffer(buffer)
 	source.Play()
 	for source.State() == openal.Playing {
-
-		//loop long enough to let the wave file finish
-
+		// wait
 	}
-	fmt.Println(source.State())
 
-	source.Pause()
-	source.Stop()
-	return
 	context.Destroy()
-	time.Sleep(time.Second)
+}
+
+func main() {
+
 }
